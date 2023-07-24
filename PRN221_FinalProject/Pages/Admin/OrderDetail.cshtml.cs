6e02a170cba2d6d03d44cfd1180245e38dfaa3e8
DESKTOP-6BICM88\dell <tkien2911@gmail.com>
2023-07-24 11:32:03 +0700

    Code Order Management

---
+++ PRN221_FinalProject/Pages/Admin/OrderDetail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221_FinalProject.Pages.Admin
+{
+    public class OrderDetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
